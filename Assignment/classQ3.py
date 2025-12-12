0a9482112943edf9a6ca97c878d9236b367e01e1
pramod garje <pramodgarje08@gmail.com>
2025-12-12 12:40:27 +0530

    day 1 python assignment

---
+++ Assignment/classQ3.py
@@ -0,0 +1,7 @@
+num1=input("Enter first number: ")
+num2=input("Enter second number: ")
+temp=int(num1)
+num1=int(num2)
+num2=temp
+print("before swapping: num1=",temp," num2=",num1)
+print("after swapping: num1=",num1," num2=",num2)
---
+++ Assignment/classQ4.py
@@ -0,0 +1,10 @@
+num1=10
+num2=20
+sum=num1+num2
+print("Sum of",num1,"and",num2,"is",sum)
+sub=num2-num1
+print("Subtraction of",num2,"and",num1,"is",sub)
+mul=num1*num2
+print("Multiplication of",num1,"and",num2,"is",mul)
+div=num2/num1
+print("Division of",num2,"by",num1,"is",div)
